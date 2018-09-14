4865096ea6ce0f0f39ac19099db0aa7a0f3d8f8f
pfalencar <patricia.alencar@etec.sp.gov.br>
2018-09-13 21:44:14 -0300

    Formulario 13-09-18

---
+++ js/testeform.js
@@ -0,0 +1,46 @@
+function VerificaCPF () {
+if (vercpf(document.frmcpf.cpf.value)) 
+{document.frmcpf.submit();}else 
+{errors="1";if (errors) alert('CPF NÃO VÁLIDO');
+document.retorno = (errors == '');}}
+function vercpf (cpf) 
+{if (cpf.length != 11 || cpf == "00000000000" || cpf == "11111111111" || cpf == "22222222222" || cpf == "33333333333" || cpf == "44444444444" || cpf == "55555555555" || cpf == "66666666666" || cpf == "77777777777" || cpf == "88888888888" || cpf == "99999999999")
+return false;
+add = 0;
+for (i=0; i < 9; i ++)
+add += parseInt(cpf.charAt(i)) * (10 - i);
+rev = 11 - (add % 11);
+if (rev == 10 || rev == 11)
+rev = 0;
+if (rev != parseInt(cpf.charAt(9)))
+return false;
+add = 0;
+for (i = 0; i < 10; i ++)
+add += parseInt(cpf.charAt(i)) * (11 - i);
+rev = 11 - (add % 11);
+if (rev == 10 || rev == 11)
+rev = 0;
+if (rev != parseInt(cpf.charAt(10)))
+return false;
+alert('O CPF INFORMADO É VÁLIDO.');return true;
+}
+
+
+function formatar(mascara, documento){
+  var i = documento.value.length;
+  var saida = mascara.substring(0,1);
+  var texto = mascara.substring(i)
+  
+  if (texto.substring(0,1) != saida){
+            documento.value += texto.substring(0,1);
+  }
+  
+}
+	
+function MascaraCPF(cpf){
+        if(mascaraInteiro(cpf)==false){
+                event.returnValue = false;
+        }       
+        return formataCampo(cpf, '000.000.000-00', event);
+}
+
